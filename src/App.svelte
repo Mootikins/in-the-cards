15641c666773cfdfac034c4877a82ac8a5cefa21
Matthew Krohn <matthew.d.krohn@gmail.com>
2021-03-12 02:48:43 -0700

    First real commit with some basic svelte layout

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import './index.css';
+	import Menu from './Menu.svelte';
+	let vh: number = window.innerHeight * 0.01;
+</script>
+
+<svelte:window
+	on:resize={() => {
+		vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	}}
+/>
+
+<div class="App">
+	<Menu />
+	<div class="play-card">Play</div>
+	<div class="hand">Hand</div>
+</div>
+
+<style type="text/scss">
+	:global(*) {
+		padding: 0;
+		margin: 0;
+	}
+
+	.App {
+		position: relative;
+		height: 100vh;
+		height: calc(var(--vh, 1vh) * 100);
+		font-family: Product Sans, Arial, Helvetica, sans-serif;
+		overflow: hidden;
+
+		.play-card {
+			font-size: 3rem;
+			font-weight: 700;
+			height: calc(var(--vh, 1vh) * 50);
+		}
+
+		.hand {
+			font-size: 3rem;
+			font-weight: 700;
+			height: calc(var(--vh, 1vh) * 50);
+			background-color: black;
+			color: white;
+		}
+	}
+</style>
---
+++ src/Menu.svelte
@@ -0,0 +1,36 @@
+<script lang="typescript">
+	import { fly } from 'svelte/transition';
+	let sidebarOpen: boolean = true;
+</script>
+
+<button on:click={() => (sidebarOpen = !sidebarOpen)}>
+	{sidebarOpen ? 'Close Menu' : 'Open Menu'}
+</button>
+
+{#if sidebarOpen}
+	<div transition:fly={{ x: 384 }} class="menu">
+		Menu
+	</div>
+{/if}
+
+<style type="text/scss">
+	.menu {
+		position: absolute;
+		height: calc(var(--vh, 1vh) * 100);
+		color: white;
+		background-color: hsl(0, 0, 15%);
+		width: 24rem;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		font-size: 3rem;
+		font-weight: 700;
+	}
+
+	button {
+		position: absolute;
+		top: 1rem;
+		right: 1rem;
+		z-index: 10;
+	}
+</style>
